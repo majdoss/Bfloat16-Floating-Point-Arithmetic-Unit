568590fce93105759ee567245526a734b3ff04ec
majdoss <mmo15@mail.aub.edu>
2022-01-12 15:39:58 +0200

    add/sub/mult/fmadd/fsub use the same circuit, we will later add a deeper pipeline

--- Source/mux_funct5.vhd
+++ Source/mux_funct5.vhd
@@ -4,9 +4,7 @@ use ieee.numeric_std.all;
 
 entity mux_funct5 is
     port(
-        add_sub: in std_logic_vector(15 downto 0) ;
-        fmadd_fmsub: in std_logic_vector(15 downto 0) ;
-        mult: in std_logic_vector(15 downto 0) ;
+        mult_add_sub: in std_logic_vector(15 downto 0) ;
         div: in std_logic_vector(15 downto 0) ;
         funct5: in std_logic_vector(4 downto 0) ;
         result: out std_logic_vector(15 downto 0)
@@ -16,9 +14,7 @@ end mux_funct5;
 architecture rtl of mux_funct5 is
 begin
     with funct5 select
-        result <= add_sub when "00000"|"00001",
-                  mult when "00010",
+        result <= mult_add_sub when "00000"|"00001"|"00010"|"00101"|"00100",
                   div when "00011",
-                  fmadd_fmsub when "00100"|"00101",
                   (others => '0') when others;
 end architecture;
